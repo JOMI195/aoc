5e468c4f7b787edc8798478f530a2b84d58bdbfb
JOMI195 <johannes.middelberg@gmx.de>
2023-12-08 22:23:51 +0100

    day2

--- src/2023/02/day2.cpp
+++ src/2023/02/day2.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <map>
 #include <string>
+#include <cassert>
 
 #include "stringparser.h"
 #include "filehandler.h"
 
-int main() {
-    FileHandler fileHandler("input.txt");
-    std::vector<std::string> input = StringParser::toVector(fileHandler.getBuffer(), "\n");
+int part1(const std::vector<std::string>& input) {
 
     int red = 12, green = 13, blue = 14;
 
@@ -19,9 +17,8 @@ int main() {
         size_t game_id_end = game.find(':');
         std::string str_game_id = game.substr(game_id_start, game_id_end);
         int game_id = std::stoi(str_game_id);
-        std::cout << game_id << "\n";
+        //std::cout << game_id << "\n";
 
-        // games
         std::string games = game.substr(game_id_end + 2, game.size());
         std::vector<std::string> sep_games = StringParser::toVector(games, "; ");
         bool validGame = true;
@@ -32,7 +29,7 @@ int main() {
             {
                 std::vector<std::string> cube = StringParser::toVector(set, " ");
                 int amount = std::stoi(cube[0]);
-                std::cout << amount << " " << cube[1] <<  "\n";
+                //std::cout << amount << " " << cube[1] <<  "\n";
                 if (cube[1] == "blue" && amount > blue || cube[1] == "red" && amount > red || cube[1] == "green" && amount > green)
                 {
                     validGame = false;
@@ -48,7 +45,19 @@ int main() {
         {
             sumed_ids += game_id;
         }
-        std::cout << sumed_ids << "\n";
+        //std::cout << sumed_ids << "\n";
     }
+    return sumed_ids;
+}
+
+int main()
+{
+    FileHandler fileHandlerTest("src/2023/02/testinput.txt");
+    std::vector<std::string> testInput = StringParser::toVector(fileHandlerTest.getBuffer(), "\n");
+    assert(part1(testInput) == 8);
+
+    FileHandler fileHandler("src/2023/02/input.txt");
+    std::vector<std::string> input = StringParser::toVector(fileHandler.getBuffer(), "\n");
+    std::cout << "Puzzle Solution Day 2 - Part 1: " << part1(input) << "\n";
     return 0;
 }
